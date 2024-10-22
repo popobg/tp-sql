36d023776b1fb09674e9830e3d6b64c78d15b259
popobg <pauline.bouyssou1@gmail.com>
2024-10-22 18:26:40 +0200

    Small fix on case

--- tp04_bouyssou.sql
+++ tp04_bouyssou.sql
@@ -1,40 +1,40 @@
 USE compta2;
 
 -- lister tous les enregistrements de la table article.
-SELECT * FROM ARTICLE;
+SELECT * FROM article;
 
 -- lister les ref et désignation pour des articles dont le prix est strictement supérieur à 2€.
 -- Tri arbitraire par ordre alphabétique à partir de la colonne 'désignation'
-SELECT REF, DESIGNATION FROM ARTICLE
+SELECT REF, DESIGNATION FROM article
 WHERE PRIX > 2
 ORDER BY DESIGNATION;
 
 -- lister les articles dont le prix est compris entre 2 et 6.25€ (inclus), avec les opérateurs de comparaison.
-SELECT * FROM ARTICLE
+SELECT * FROM article
 WHERE PRIX >= 2 AND PRIX <= 6.25;
 
 -- lister les articles dont le prix est compris entre 2 et 6.25€ (inclus), avec BETWEEN.
-SELECT * FROM ARTICLE
+SELECT * FROM article
 WHERE PRIX BETWEEN 2 AND 6.25;
 
 -- lister les articles dont le prix N'EST PAS COMPRIS entre 2 et 6.25€
 -- et dont le fournisseur est 'Française d'Imports' (ID = 1).
-SELECT * FROM ARTICLE
+SELECT * FROM article
 WHERE !(PRIX >= 2 AND PRIX <= 6.25)
 AND ID_FOU = 1;
 
 -- lister les articles dont le fournisseur est 'Française d'Imports' ou 'Dubois & fils' (ID 1 et 3).
 -- avec un opérateur logique
-SELECT * FROM ARTICLE
+SELECT * FROM article
 WHERE ID_FOU = 1 OR ID_FOU = 3;
 
 -- lister les articles dont le fournisseur est 'Française d'Imports' ou 'Dubois & fils' (ID 1 et 3).
 -- avec l'opérateur IN
-SELECT * FROM ARTICLE
+SELECT * FROM article
 WHERE ID_FOU IN (1, 3);
 
 -- lister les articles dont le fournisseur n'est ni 'Française d'Imports', ni 'Dubois & fils'.
-SELECT * FROM ARTICLE
+SELECT * FROM article
 WHERE ID_FOU NOT IN (1, 3);
 
 -- lister tous les bons de commande dont la date de commande est entre le 01/02/19 et le 30/04/19.
